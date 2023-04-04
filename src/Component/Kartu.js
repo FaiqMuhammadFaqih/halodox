fc09b1a61978c2226f938ce72c41b6d66f0b9ecb
Faiq Muhammad Faqih <faiqmuhammadfaqih3@gmail.com>
2023-04-04 10:21:54 +0700

    commitan ke 4

--- src/Component/Kartu.js
+++ src/Component/Kartu.js
@@ -5,40 +5,13 @@ import Card from 'react-bootstrap/Card';
 
 const Kartu = (props) => {
     return(
-      <div>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={props.img2} />
+      
+        <Card style={{ display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", borderRadius:"5px", border:"1px solid #657d74", paddingbottom:"5px" }}>
+          <Card.Img  src={props.image} style={{width:"45%"}} />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title style={{fontSize:"14px"}} >{props.teks}</Card.Title>
           </Card.Body>
         </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={props.img3} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={props.img4} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={props.img5} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={props.img6} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-          </Card.Body>
-        </Card>
-      </div>
-    
-
     )
 }
 
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -5,40 +5,42 @@ import image3 from "./../Assets/download (1).png"
 import image4 from "./../Assets/download (2).png"
 import image5 from "./../Assets/download (3).png"
 import image6 from "./../Assets/download (4).png"
+import LogoImage from "./../Assets/home-banner-v3.png"
 
 import Kartu from "./Kartu";
 
 const Main = () => {
     return(
-        <div>
+        <div >
             <Navig img1={image}/>
-
-            <div className="container">
+            <div style={{backgroundImage: `url(${LogoImage})`, backgroundRepeat:"no-repeat", margin:"0", backgroundPosition:"bottom right", backgroundSize:"cover", paddingBottom:"100px"}}>
+            <div className="container" >
                 <div className="row"style={{margin:"15px"}}>
-                    <h1  style={{fontSize:"32px", color:"#494243", paddingTop:"50px"}}>Solusi Kesehatan Terlengkap</h1>
+                    <h1  style={{fontSize:"32px", color:"#2d2f2e", paddingTop:"50px", fontWeight:"700"}}>Solusi Kesehatan Terlengkap</h1>
                     <p style={{width:"650px", color:"#8a8082"}}>Chat dokter, kunjungi rumah sakit, beli obat, cek lab dan update informasi seputar kesehatan, semua bisa di Halodoc!</p>
                 </div>
-                <div className="row">
-                    <div>
-                        <Kartu img2={image2}/>
+                <div className="row gap-3 mb-5 m-3">
+                    <div className="col-2">
+                        <Kartu image={image2} teks="Chat Dengan Docter"/>
                     </div>
-                    <div>
-                        <Kartu img3={image3}/>
+                    <div className="col-2">
+                        <Kartu image={image3} teks="Toko Kesehatan"/>
                     </div>
-                    <div>
-                        <Kartu img4={image4}/>
+                    <div className="col-2">
+                        <Kartu image={image4} teks="Buat Janji RS"/>
                     </div>
-                    <div>
-                        <Kartu img5={image5}/>
+                    <div className="col-2">
+                        <Kartu image={image5} teks="Janji Medis"/>
                     </div>
-                    <div>
-                        <Kartu img6={image6}/>
+                    <div className="col-2">
+                        <Kartu image={image6} teks="Layanan Home Lab"/>
                     </div>
-                
-                
-                
-                
+                    <div style={{width:"35%"}}>
+                        <Kartu/>
+                    </div>
                 </div>
+               
+            </div>
             </div>
         </div>
     )
